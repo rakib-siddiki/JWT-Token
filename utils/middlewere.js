3a291ec2297279c747d7829d82326cb8234bca91
rakib-siddiki <rmrakib901@gmail.com>
2023-12-24 14:08:50 +0600

    fix typo err json

--- utils/middlewere.js
+++ utils/middlewere.js
@@ -7,9 +7,9 @@ async function TestMiddleWere(req, res, next) {
    !token && res.status(401).json({ message: "unauthorized user" });
   const decoded = jwt.verify(token,"email")
   const user = await userlist.findOne({email:decoded.email})
-  user ? next() : res.status(401), json("unauthorized user");
+  user ? next() : res.status(401).json("unauthorized user");
  } catch (error) {
-  console.error(error);
+  console.error(error.message);
   res.status(401).json({message:"unauthorized user"})
  }
 
